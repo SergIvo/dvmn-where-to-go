ac4537fc84fe0b84a460616f75a1dde248de9f13
Sergey <Sergey@localhost.localdomain>
2023-01-13 11:54:09 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -8,6 +8,14 @@ class Place(models.Model):
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
 
-
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Место', related_name='images')
+    image = models.ImageField(verbose_name='Изображение')
+    number = models.IntegerField(verbose_name='Порядковый номер изображения')
+
+    def __str__(self):
+        return f'{self.number} {self.place.title}'
