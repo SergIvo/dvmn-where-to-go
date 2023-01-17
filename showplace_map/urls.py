85fc5fda2b737036c042101657d9ae6b942f47e7
Sergey <Sergey@localhost.localdomain>
2023-01-17 23:33:01 +0300

    Add ending slash

--- showplace_map/urls.py
+++ showplace_map/urls.py
@@ -7,6 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.render_map),
-    path('places/<int:id>', views.send_place_details, name='places'),
+    path('places/<int:id>/', views.send_place_details, name='places'),
     path('tinymce/', include('tinymce.urls'))
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
